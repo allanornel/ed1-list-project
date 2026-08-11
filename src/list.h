daae7410eacf735ee0831d72a87a0e922b22e70d
allanornel <allanornel@gmail.com>
2026-08-10 21:28:03 -0300

    feat: implement all methods on list.h

--- src/list.h
+++ src/list.h
@@ -1,9 +1,26 @@
 #ifndef LIST_H
 #define LIST_H
 
+// basic operations
+void cleanList();
+void isEmptyList();
+void isFullList();
+void getListSize();
+
+// insert
 void add(int value);
-void remove(int position);
+void addAtStart(int value);
+void addAtPosition(int position, int value);
+
+// remove
+void removeFirstPosition();
+void removeLastPosition();
+void removeByPosition(int position);
+void removeByValue(int value);
+
+// find
 void find(int position);
+void findValuePositions(int searchedValue);
 void printList(void);
 
 #endif
